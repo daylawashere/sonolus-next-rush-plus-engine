179d07d81ff0cece9c49e27907b6e38c08d34a7f
Dayla <undertalefan622@gmail.com>
2026-04-23 09:52:46 -0600

    adjust raw number values

--- sekai/lib/events.py
+++ sekai/lib/events.py
@@ -41,7 +41,7 @@ from sekai.lib.level_config import LevelConfig
 from sekai.lib.options import Options, SkillMode, Version
 from sekai.lib.particle import ActiveParticles
 from sekai.lib.skin import ActiveSkin
-from sonolus.script.easing import ease_out_quad
+from sonolus.script.easing import ease_out_quad, ease_in_quad
 
 
 @level_memory
@@ -284,7 +284,7 @@ def draw_skill_bar(draw_time: float, num: int, effect: SkillMode, level: int):
     enter_progress = unlerp_clamped(0, 0.25, draw_time)
     exit_progress = unlerp_clamped(3.75, 4, draw_time)
 
-    anim = ease_out_quad(enter_progress - exit_progress)
+    anim = ease_out_quad(enter_progress - exit_progress) if time <= 2 else ease_in_quad(enter_progress - exit_progress)
 
     layout = +Quad
     x_ratio = 0
@@ -353,7 +353,7 @@ def draw_skill_bar(draw_time: float, num: int, effect: SkillMode, level: int):
             if draw_time < 1.75:
                 final_anim = ease_out_quad(enter_progress)
             else:
-                final_anim = ease_out_quad(mid_progress - exit_progress)
+                final_anim = ease_in_quad(mid_progress - exit_progress)
         text_current_center = lerp(current_start_pos, text_target_center, final_anim)
         h = 0.027
         w = h * 14
