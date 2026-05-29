d901fe1f95568cb93e12aa9f1bae2fd4e6f836ea
Dayla <undertalefan622@gmail.com>
2026-05-29 11:20:17 -0600

    dont play lane particle after song end

--- sekai/lib/stage.py
+++ sekai/lib/stage.py
@@ -69,7 +69,7 @@ from sekai.lib.skin import (
     LifeBarType,
     ScoreRankType,
 )
-
+from sekai.lib.initialization import LastNote
 
 class JudgeLineColor(IntEnum):
     NEUTRAL = 0
@@ -1129,5 +1129,8 @@ def schedule_lane_sfx(lane: float, target_time: float):
 
 def play_lane_particle(lane: float):
     if Options.lane_effect_enabled:
-        layout = layout_particle_lane(lane, 0.5)
-        ActiveParticles.lane.spawn(layout, duration=0.3 / Options.effect_animation_speed)
+        if LastNote.last_time < time() and is_play():
+            return
+        else:
+            layout = layout_particle_lane(lane, 0.5)
+            ActiveParticles.lane.spawn(layout, duration=0.3 / Options.effect_animation_speed)
